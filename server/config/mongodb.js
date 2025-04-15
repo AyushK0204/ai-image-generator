3fd24fbe92fd6635efb06674e70568828a5fc604
AyushK0204 <ayu.k.210402@gmail.com>
2025-04-15 19:59:17 +0530

    Added userController, auth middleware, userModel, userRoutes, generateToken as utils and updated server file

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("Database Connected Successfully");
   });
-  await mongoose.connect(`${process.env.MONGODB_URI}/picolo`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/piccolo`);
 };
 
 export default connectDB;
---
+++ server/controllers/userController.js
@@ -0,0 +1,96 @@
+import userModel from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import generateToken from "../utils/generateToken.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.json({
+        success: false,
+        message: "Missing Credentials!",
+      });
+    }
+    const userExists = await userModel.findOne({ email });
+    if (userExists) {
+      return res.json({
+        success: false,
+        message: "User already registered!",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+
+    res.json({
+      success: true,
+      token: generateToken(user._id),
+      user: {
+        name: user.name,
+      },
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      res.json({
+        success: true,
+        token: generateToken(user._id),
+        user: {
+          name: user.name,
+        },
+      });
+    } else {
+      return res.json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const userCredits = async (req, res) => {
+  try {
+    const { userId } = req;
+    const user = await userModel.findById(userId);
+    res.json({
+      success: true,
+      credits: user.creditBalance,
+      user: { name: user.name },
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
